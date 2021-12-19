2e78c2ba7735af933260c40e00219e023d62fed4
Nguyễn Chính Nghiệp <72908486+Nghiep-NguyenChinh@users.noreply.github.com>
2021-12-19 23:05:43 +0700

    Update app_imagecaption2.py

--- app_imagecaption2.py
+++ app_imagecaption2.py
@@ -8,8 +8,8 @@ import search_system
 
 import gdown
 # data flick30k
-url_data = "https://drive.google.com/uc?id=1KQOWoQAq9cvKKurhC00zqGfTfLFtIUw2"
-output_data = "flickr30k.zip"
+# url_data = "https://drive.google.com/uc?id=1KQOWoQAq9cvKKurhC00zqGfTfLFtIUw2"
+# output_data = "flickr30k.zip"
 
 # predict_flicrk30k
 url_json = "https://drive.google.com/uc?id=1175qCLlLyI5pC0iXUrltAdqfMR0AvCkF"
@@ -17,6 +17,11 @@ root = "predict_flicrk30k.json"
 
 url_search_sys =  "https://drive.google.com/uc?id=16gUqEan81aq2SY4Se4wBX3d6WRZ9KSjn"
 search_sys ="Search_Sys.zip"
+
+
+import wget
+url_data = "https://public.boxcloud.com/d/1/b1!JMCSfCZ4dOsyNQ4u3nka075n7AA0iDmvyveKgNMzb0OG6iqPj9MBsaa2Q1BpB_u-sTy4gnhhExo-7MjqmloUTtbQajsUDReiWhNpLxg5W4lk7da0AhJmCt1IxnEgWxqVUYfEeU715j8CUqUR0eQlmbyzVjZJppunzOuaYoOQ5WFM3YKSXCGC6Avk1ugbD-Gc3ImK0i2i-_iEeTH0TqzJeb6d9Z3movyNbEIBI42dwIun430879H7LnpIT9NkatvPcz2Qd1JeOPDzTmWbAw4BPUhxYml_-v2gSVijMZOAoE7P0z-SkT854L2wpk9gdGbRxE0Z2ohGPl1lrfdB4IM1wUmGX53VD-72SZPHOq7OTl-u1crD5UKqy_MtQ8SS1D14_CM3ieeqZ09uxKVgKWRDnpQrtmPepO6iNSYMKU0VNLq0ayTbfSPV26-Pnv9IBXn8T30YTYoxpUM8zwzcpT6ehUDBxSq8aYG_jV8NTfHABDMoFlk_03X70XWjC3JJQVms-G5DjXYlxE6u2hmN5eTx1pL-bqsBNP2o4-xtW88a_Rt1dcuewOUQNxeF1R06KI-X99Fgjr0q_JUN7-kGyaIen8Q1EJPRnqT563cJ2BD4wZ557dcTr2_CjAS0SilrNAjx2070R32fXDgIg714dqWrGXxkySH5yxrDNCvSUtXc_oEntkmGiSWUx2FmqawYfu1YnFz2nc7KkfBH8HKtpgPXje5TPUuzyvlt9M-NQSWcCQaB1pMy2o48BRtHSqCKgjHCeZJExdhim6JTdG5NLHgA5-RnRBZj_U8Dw53Sx9qTUIZpWge1EckUQPXs4mVRdJtIvuSBKq42c8N8pgK6RECz5ZLcpH1eLcFeHrdIcrKirl0aXarqBSF7rl-sDtglfX2d-fQcREIdYKXoULGOJd19BpZboXoWiCPtYwkBd3uIQUcBwVEiT6nb_L95HDsr4uEeN3gduupo9R4XnI08I5503WcWO2Q31a_xRQLil9hxALjlPeDPFinC0R3FYN_ZS9ooPdRrFLex817fPk7sJfq54TumERT22PKVcaaCx3lEU2-ERMZMSm0CAV5_8XTD5s6a2va2MZmdA6g9QAfbaAc_hk6s0VHQ35jQfJBf1qf5MHRlXkg7GX4BHIAkbSNV7K7WJ_MH1oARdeJLK64JDi3P_6fulFHSzIgSWR2bVIZiZ6g6Qv7mqSF2BLJ08BBlANDWtDXhNLeiapn1MzPkpr4kNix0bHuNySp_SZzQWI4zK0To7v7QoXQW82g33bvgeBWdfP-dBECxsxzMILQXR0JV0TW0ZfQ8SP8tLoQP6VKiQUoov2k6G--r9PNjMSQzrxH8o8Dythhd/download"
+output_data = "flickr30k.zip"
 
 #########################################################
 ##### GIAO DIỆN
@@ -39,8 +44,13 @@ st.write("Bắt đầu dowload")
 def download_data(url, output):      
     if (os.path.exists(output)==False):
         gdown.download(url, output, quiet=False)
+
+def download_data_2(url, output):      
+    if (os.path.exists(output)==False):
+        wget.download(url)
 #tai data_flick30k.zip
-download_data(url_data, output_data)
+# download_data(url_data, output_data)
+download_data_2(url_data, output_data)
 download_data(url_json, root)
 download_data(url_search_sys, search_sys)
 
